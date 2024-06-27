bd2745a975c3806625207c732d03cd49aa4a9ce2
Derek327783 <derekng406@gmail.com>
2024-06-27 19:46:03 +0800

    Added FT files

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Upload your audio files and get back a image for inspiration!")
+st.caption("Upload audio files to get text/images or upload text to get audio/images")
