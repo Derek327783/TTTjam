23893c391a76a5e9264f2541e940cd2242383a31
Derek327783 <derekng406@gmail.com>
2024-06-27 21:45:09 +0800

    added clap sandbox

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("Upload your audio files and get back a image for inspiration!")
-st.caption("Upload audio files to get text/images or upload text to get audio/images")
+st.title("Upload your audio files and get an image for inspiration!")
+uploaded_file = st.file_uploader("Upload an audio file",type =["wav"],key="Audio_file_input")
