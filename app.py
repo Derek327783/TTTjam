67f6328ebef92c501b42985181e274737e973d97
Derek327783 <derekng406@gmail.com>
2024-06-28 16:53:12 +0800

    Added predict file for Base_CNN_classifier

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.title("Upload your audio files and get an image for inspiration!")
 uploaded_file = st.file_uploader("Upload an audio file",type =["wav"],key="Audio_file_input")
+if st.button("Get tempo"):
+    st.caption("110bpm")
+if st.button("Get a textual description of your audio"):
+    st.caption("I am now deaf")
+if st.button("Get image inspiration"):
+    st.caption("You do not want to see what image I came up with.")
+
