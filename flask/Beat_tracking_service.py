77892b6804461a81d09b6a660b1c18f7506c9cbe
Derek327783 <derekng406@gmail.com>
2024-07-01 14:28:26 +0800

    Added inference files

--- flask/Beat_tracking_service.py
+++ flask/Beat_tracking_service.py
@@ -1,8 +1,18 @@
 import librosa
 
-def beat_getter(uploaded_file):
-    y,sr = librosa.load(uploaded_file)
-    beat = librosa.beat.beat_track(y=y,sr=sr)
-    return beat
+class _Beat_tracking_service():
+    instance = None
+    def get_beat(self,file_path):
+        y, sr = librosa.load(file_path)
+        beat = librosa.beat.beat_track(y=y, sr=sr)
+        return beat[0][0]
 
-print(beat_getter("metal.00003.wav"))
+def Beat_tracking_service():
+    if _Beat_tracking_service.instance == None:
+        _Beat_tracking_service.instance = _Beat_tracking_service()
+    return _Beat_tracking_service.instance
+
+if __name__ == "__main__":
+    emotion_service = Beat_tracking_service()
+    predicted_beat = emotion_service.get_beat("10.mp3")
+    print(predicted_beat)
--- flask/Genre_spotting_service.py
+++ flask/Genre_spotting_service.py
@@ -3,58 +3,48 @@ import numpy as np
 import tensorflow as tf
 import argparse
 import json
+import tensorflow.keras as keras
+
 # Input dimension for model = 259*13
 
 mappings = ["blues","classical","country","disco","hip-hop","jazz","metal","pop","reggae","rock"]
-model_path = "flask/Genre_classifier_model.h5"
-def preprocess_audio(file_path, target_frames=259, n_mfcc=13, sr=22050, n_fft=2048, hop_length=512):
+model_path = "Genre_classifier_model.h5"
+class _Genre_spotting_service():
+    instance = None
+    model = None
+    mappings = ["blues", "classical", "country", "disco", "hip-hop", "jazz", "metal", "pop", "reggae", "rock"]
+
+    def predict(self,file_path):
+        model = tf.keras.models.load_model(model_path)
+        input_data = self.preprocess_audio(file_path)
+        input_data = input_data[np.newaxis, ...]
+        predictions = model.predict(input_data)
+        text_predictions = [mappings[np.argmax(predictions, axis=1)[0]]]
+        return text_predictions
+
+    def preprocess_audio(self,file_path, target_frames=259, n_mfcc=13, sr=22050, n_fft=2048, hop_length=512):
     # Load audio file
-    y, sr = librosa.load(file_path, sr=sr)
+        y, sr = librosa.load(file_path, sr=sr)
 
     # Compute MFCCs
-    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length)
+        mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length)
 
     # Pad or truncate MFCCs to ensure they have target_frames
-    if mfccs.shape[1] < target_frames:
-        pad_width = target_frames - mfccs.shape[1]
-        mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
-    else:
-        mfccs = mfccs[:, :target_frames]
-    return mfccs.T
+        if mfccs.shape[1] < target_frames:
+            pad_width = target_frames - mfccs.shape[1]
+            mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
+        else:
+            mfccs = mfccs[:, :target_frames]
+        return mfccs.T
 
-def load_model(model_path):
-    model = tf.keras.models.load_model(model_path)
-    return model
 
-def run_inference(raw_data):
-    model = load_model(model_path)
-    input_data = preprocess_audio(raw_data)
-    input_data = input_data[np.newaxis, ...]
-    predictions = model.predict(input_data)
-    text_predictions = [mappings[np.argmax(predictions, axis=1)[0]]]
-    return text_predictions
+def Genre_spotting_service():
+    if _Genre_spotting_service.instance == None:
+        _Genre_spotting_service.instance = _Genre_spotting_service()
+        _Genre_spotting_service.model = keras.models.load_model(model_path)
+    return _Genre_spotting_service.instance
 
-# model = load_model(model_path)
-# input_data = preprocess_audio("metal.00003.wav")
-# predictions = run_inference(model, input_data)
-# text_predictions = [mappings[np.argmax(predictions, axis=1)[0]]]
-# with open("output.json", 'w') as f:
-#     json.dump(text_predictions, f)
-
-# def main():
-#     parser = argparse.ArgumentParser(description='Run inference on a TensorFlow model.')
-#     parser.add_argument('--input_path', type=str, required=True, help='Path to the input data JSON file.')
-#     parser.add_argument('--output_path', type=str, required=True, help='Path to save the inference results.')
-#
-#     args = parser.parse_args()
-#     model = load_model(model_path)
-#     input_data = preprocess_audio("metal.00003.wav")
-#     predictions = run_inference(model, input_data)
-#     text_predictions = list[mappings[np.argmax(predictions, axis=1)[0]]]
-#     with open(args.output_path, 'w') as f:
-#         json.dump(text_predictions, f)
-#
-#     print(f'Inference results saved to {args.output_path}')
-#
-# if __name__ == '__main__':
-#     main()
+if __name__ == "__main__":
+    genre_service = Genre_spotting_service()
+    predicted_genre = genre_service.predict("10.mp3")
+    print(predicted_genre)
